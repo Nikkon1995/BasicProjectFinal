a91c37bea1cea3fc623f39204b8a8eaa69bbc246
Nikkon1995 <dutta.nikkon@gmail.com>
2021-07-06 22:55:50 +0530

    Added support for tabbing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @Output() featureEvent = new EventEmitter<string>();
+  onSelect(item: string){
+    if(item === 'recipe') {
+      this.featureEvent.emit(item);
+    } else if(item === 'shopping-list') {
+      this.featureEvent.emit(item);
+    }
+  }
 }
