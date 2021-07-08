97265b93652898a9fadd7afff00027ff01d9a560
Nikkon1995 <dutta.nikkon@gmail.com>
2021-07-08 23:55:47 +0530

    Added functionality to add ingredients into shopping lists from Selected Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,34 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe 1', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Recipe 2', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Tasty Schnitzel', 
+        'A super tasty Schnitzel - Just Awesome', 
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+        [
+            new Ingredient('Meat',1),
+            new Ingredient('French Fries',20)
+        ]),
+        new Recipe('Big Fat Burger', 
+        'What more do you need to say?', 
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Burger_King_Big_King_XXL_Menu.jpg/800px-Burger_King_Big_King_XXL_Menu.jpg',
+        [
+            new Ingredient('Buns',2),
+            new Ingredient('Meat',1)
+        ])
       ];
 
+    constructor(private slService: ShoppingListService) {}
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredientsToSLFromRecipe(ingredients);
     }
 }
